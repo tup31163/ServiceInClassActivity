40a115e2e749f842f91b3e430480734d91365d46
Kenny Miller <63763541+knnymllr@users.noreply.github.com>
2023-11-09 14:46:30 -0500

    completed service connection, countdown working in logcat

--- app/src/main/java/edu/temple/myapplication/MainActivity.kt
+++ app/src/main/java/edu/temple/myapplication/MainActivity.kt
@@ -1,25 +1,47 @@
 package edu.temple.myapplication
 
+import android.content.ComponentName
+import android.content.Intent
+import android.content.ServiceConnection
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.IBinder
 import android.widget.Button
+import android.widget.TextView
 
+
+lateinit var timerBinder : TimerService.TimerBinder
+
+var isConnected = false;
 class MainActivity : AppCompatActivity() {
+
+    val serviceConnection = object : ServiceConnection {
+        override fun onServiceConnected(name: ComponentName?, service: IBinder?) {
+            timerBinder = service as TimerService.TimerBinder
+            isConnected = true
+        }
+
+        override fun onServiceDisconnected(name: ComponentName?) {
+            isConnected = false
+        }
+    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        findViewById<Button>(R.id.startButton).setOnClickListener {
+        bindService(Intent(this, TimerService::class.java), serviceConnection, BIND_AUTO_CREATE)
 
+        findViewById<Button>(R.id.startButton).setOnClickListener {
+            if (isConnected) timerBinder.start(100)
         }
 
         findViewById<Button>(R.id.pauseButton).setOnClickListener {
-
+            if (isConnected) timerBinder.pause()
         }
         
         findViewById<Button>(R.id.stopButton).setOnClickListener {
-
+            if (isConnected) timerBinder.stop()
         }
     }
 }
