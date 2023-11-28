be2a1c7c8c89f55331c8dd71bda68f6da619a7ad
Kenny Miller <63763541+knnymllr@users.noreply.github.com>
2023-11-28 14:46:01 -0500

    transition onClickListeners to onOptionsItemSelected() for menu items, both sets of buttons work

--- app/src/main/java/edu/temple/myapplication/MainActivity.kt
+++ app/src/main/java/edu/temple/myapplication/MainActivity.kt
@@ -8,6 +8,8 @@ import android.os.Bundle
 import android.os.Handler
 import android.os.IBinder
 import android.os.Looper
+import android.view.Menu
+import android.view.MenuItem
 import android.widget.Button
 import android.widget.TextView
 
@@ -15,6 +17,25 @@ import android.widget.TextView
 
 var isConnected = false;
 class MainActivity : AppCompatActivity() {
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+
+        menuInflater.inflate(R.menu.controls, menu)
+
+        return super.onCreateOptionsMenu(menu)
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+
+        when (item.itemId) {
+            R.id.action_start -> if (isConnected) timerBinder.start(100)
+            R.id.action_pause -> if (isConnected) timerBinder.pause()
+            R.id.action_stop -> if (isConnected) timerBinder.stop()
+        }
+
+        return super.onOptionsItemSelected(item)
+    }
+
 
     lateinit var timerText : TextView
     lateinit var timerBinder : TimerService.TimerBinder
